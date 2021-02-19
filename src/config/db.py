0cdd42a2d28a0a687b6d1782926008989ca41848
Jeyson Calvache <jeison130@gmail.com>
2021-02-18 22:21:43 -0500

    Agregando conexion a mariadb y modelo productos

---
+++ src/config/db.py
@@ -0,0 +1,11 @@
+import mariadb
+
+config = {
+    'host': 'localhost',
+    'port': 3308,
+    'user': 'root',
+    'password': '',
+    'database': 'flask_mvc'
+}
+
+DB = mariadb.connect(**config)
--- src/controllers/productos.py
+++ src/controllers/productos.py
@@ -1,5 +1,12 @@
 from src import app
+from src.models.productos import ProductosModel
 
 @app.route('/productos')
 def productos():
+    productosModel = ProductosModel()
+
+    productos = productosModel.traerTodos()
+
+    print(productos)
+
     return 'En productos'
